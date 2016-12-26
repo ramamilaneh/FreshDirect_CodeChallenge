e2f3e04ee36ac0dbc45cb992821fe615038ea590
Rama Milaneh <ramamilaneh@gmail.com>
2016-12-26 12:26:36 -0500

    added func to get the bearer token

--- FreshDirect_CodeChallenge/APIClient.cs
+++ FreshDirect_CodeChallenge/APIClient.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Foundation;
+using RestSharp;
+using Newtonsoft.Json;
+
 namespace FreshDirect_CodeChallenge
 {
 	public class APIClient
@@ -6,8 +10,9 @@ namespace FreshDirect_CodeChallenge
 
 		static string authUrl = "https://api.twitter.com/oauth2/token";
 		static string userTimelineUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
 		// Encode consumer key and secret to Base64 encode
-		public string getBase64EncodeString()
+		public static string getBase64EncodeString()
 		{
 			string concatenateKeyAndSecret = Secrets.consumerKey + ":" + Secrets.consumerSecret;
 			byte[] secretAndKeyData = System.Text.Encoding.UTF8.GetBytes(concatenateKeyAndSecret);
@@ -18,7 +23,17 @@ namespace FreshDirect_CodeChallenge
 		// Get the bearer token
 		public static void getBearerToken()
 		{
+			var client = new RestClient(authUrl);
+			var request = new RestRequest(Method.POST);
+			string key = getBase64EncodeString();
+			request.AddHeader("Authorization", "Basic " + key);
+			request.AddHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+			request.AddParameter("grant_type", "client_credentials");
+			client.ExecuteAsync(request, response =>
+			{
+				Console.WriteLine(response.Content);
 
+			});
 
 
 		}
