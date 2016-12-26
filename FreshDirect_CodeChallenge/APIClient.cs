1a9b1e526acff082c4dab18b74a917609a5381d0
Rama Milaneh <ramamilaneh@gmail.com>
2016-12-26 11:34:07 -0500

    added func to encode key ans secret string

--- FreshDirect_CodeChallenge/APIClient.cs
+++ FreshDirect_CodeChallenge/APIClient.cs
@@ -3,8 +3,25 @@ namespace FreshDirect_CodeChallenge
 {
 	public class APIClient
 	{
-		public APIClient()
+
+		static string authUrl = "https://api.twitter.com/oauth2/token";
+		static string userTimelineUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+		// Encode consumer key and secret to Base64 encode
+		public string getBase64EncodeString()
 		{
+			string concatenateKeyAndSecret = Secrets.consumerKey + ":" + Secrets.consumerSecret;
+			byte[] secretAndKeyData = System.Text.Encoding.UTF8.GetBytes(concatenateKeyAndSecret);
+			var base64EncodeKeyAndSecret = Convert.ToBase64String(secretAndKeyData);
+			return base64EncodeKeyAndSecret;
 		}
+
+		// Get the bearer token
+		public static void getBearerToken()
+		{
+
+
+
+		}
+
 	}
 }
