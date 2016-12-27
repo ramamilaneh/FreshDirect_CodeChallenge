12778186acb4054fa190891fac0ae79e35a51aa7
Rama Milaneh <ramamilaneh@gmail.com>
2016-12-27 11:38:03 -0500

    added table source

--- FreshDirect_CodeChallenge/TweetsViewController.cs
+++ FreshDirect_CodeChallenge/TweetsViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Diagnostics.Contracts;
+using Foundation;
 using UIKit;
 
 namespace FreshDirect_CodeChallenge
@@ -17,13 +18,39 @@ namespace FreshDirect_CodeChallenge
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
-			Console.WriteLine(tweetsOfUser);
+			TableView.Source = new TweetsDataSource(this);
+			Console.WriteLine(tweetsOfUser.Length);
 		}
 
 		public override void DidReceiveMemoryWarning()
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		class TweetsDataSource : UITableViewSource
+		{
+			TweetsViewController controller;
+			public TweetsDataSource(TweetsViewController controller)
+			{
+				this.controller = controller;
+			}
+
+			public override nint RowsInSection(UITableView tableview, nint section)
+			{
+				return controller.tweetsOfUser.Length;
+			}
+
+			public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+			{
+				// request a recycled cell to save memory
+				UITableViewCell cell = tableView.DequeueReusableCell("tweetCell");
+				// if there are no cells to reuse, create a new one
+				if (cell == null)
+					cell = new UITableViewCell(UITableViewCellStyle.Default, "tweetCell");
+				cell.TextLabel.Text = controller.tweetsOfUser[indexPath.Row];
+				return cell;
+			}
 		}
 	}
 }
