22ab3d7e2e89820aede38d576ad3f31ef979722f
Rama Milaneh <ramamilaneh@gmail.com>
2016-12-26 21:17:53 -0500

    added search button

--- FreshDirect_CodeChallenge/ViewController.cs
+++ FreshDirect_CodeChallenge/ViewController.cs
@@ -11,11 +11,13 @@ namespace FreshDirect_CodeChallenge
 			// Note: this .ctor should not contain any initialization logic.
 		}
 		UITextField userNameTextField = new UITextField();
+		UIButton searchButton = new UIButton();
 
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 			setupTextField();
+			setupSearchButton();
 			//APIClient.getTweets("michellebklynn", (jsonDic) =>
 			//{
 			//	Console.WriteLine(jsonDic);
@@ -34,11 +36,28 @@ namespace FreshDirect_CodeChallenge
 			userNameTextField.Frame = new CoreGraphics.CGRect(this.View.Center.X - this.View.Frame.Width / 4 , this.View.Frame.Height/3, this.View.Frame.Width / 2, 50);
 			this.View.AddSubview(userNameTextField);
 			userNameTextField.Placeholder = "Enter the screen name";
-			this.userNameTextField.ShouldReturn = delegate 
+			userNameTextField.Layer.BorderWidth = 1;
+			userNameTextField.Layer.CornerRadius = 10;
+			userNameTextField.Layer.BorderColor = UIColor.DarkGray.CGColor;
+			this.userNameTextField.ShouldReturn = delegate
 			{
 				userNameTextField.ResignFirstResponder();
 				return true;
 			};
 		}
+
+		public void setupSearchButton()
+		{
+			searchButton.Frame = new CoreGraphics.CGRect(this.View.Center.X-25, this.View.Frame.Height / 2, 50, 50);
+			this.View.AddSubview(searchButton);
+			searchButton.Layer.CornerRadius = 25;
+
+
+			searchButton.SetImage(UIImage.FromFile("Images/twitter1.png"), UIControlState.Normal);
+
+
+		}
+
+
 	}
 }
